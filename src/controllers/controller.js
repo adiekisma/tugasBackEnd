743c81e1751b5e2d808c709ca25e78847e7ed776
adiekisma <adiek.isma@gmail.com>
2023-02-09 15:04:12 +0700

    update connect to database

---
+++ src/controllers/controller.js
@@ -0,0 +1,10 @@
+exports.helloWorld = (req, res, next) => {
+  res.send('<h1>Hello World!</h1>');
+};
+
+// export.aboutMe = (req, res, next) => {
+//     res.send({
+//         nama: "Adiek",
+//         Alamat: "Surabaya"
+//     })
+// }
