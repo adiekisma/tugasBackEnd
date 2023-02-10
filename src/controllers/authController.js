b25d1449b783c2de477bd6302868504cdf0359ff
adiekisma <adiek.isma@gmail.com>
2023-02-10 14:49:22 +0700

    delete fitur

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -18,7 +18,7 @@ exports.register = async (req, res, next) => {
 
     return res.status(201).send({
       message: 'create user success',
-      result: registerUser,
+      //result: registerUser,
     });
   } catch (error) {
     return res.status(500).send(error);
@@ -65,17 +65,17 @@ exports.login = async (req, res, next) => {
 exports.deleteUser = async (req, res, next) => {
   const payload = req.body;
   try {
-    await Model.user
-      .findAll({ where: { username: payload.username } })
+    await user.destroy
+      .findAll({ where: { id: payload.id } })
       .then(async (result) => {
         if (result.length > 0) {
-          await Model.user.destroy({ where: { username: payload.username } });
+          await user.destroy({ where: { id: payload.id } });
           res.status(200).json({ message: 'delete user successfully' });
         } else {
           res.status(404).json({ message: 'username not found' });
         }
       });
   } catch (error) {
-    res.status(404).json({ message: error });
+    res.status(404).json({ error });
   }
 };
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 //router.put('/:id', authController.updateUser);
-router.delete('/', authController.deleteUser);
+router.delete('/delete', authController.deleteUser);
 
 module.exports = router;
