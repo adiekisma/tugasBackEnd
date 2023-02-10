bb9e45501c9d166f20725323b288816617d350aa
adiekisma <adiek.isma@gmail.com>
2023-02-10 16:09:46 +0700

    Update authRouter.js

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.put('/', authController.updateUser);
-router.delete('/delete', authController.deleteUser);
+router.delete('/:id', authController.deleteUser);
 
 module.exports = router;
